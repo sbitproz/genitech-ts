88a913b084314987875c4d3d34ea80332653b6f1
Dan Clovis <dan@infinatech.co.uk>
2022-03-01 18:06:08 +0000

    mock data generator

--- src/util/commands/mock.helpers.ts
+++ src/util/commands/mock.helpers.ts
@@ -1,30 +1,43 @@
 import { MODULE } from "@config/module.constants";
 import { Config, Schema } from "@interfaces/buildBase.interface";
-import { GenerateReturn, GeneratorEntity } from "@interfaces/template.interface";
+import {
+  GenerateReturn,
+  GeneratorEntity,
+} from "@interfaces/template.interface";
 
-export const mockGeneratorEntity = (Generator: GeneratorEntity) => (config: Config) => ({
-  title: 'Mock Data',
-  fileName: `${MODULE.MOCK}/${config.name}mock.json`,
-  template: config.entities
-    .map((entity) => ({ entityName: entity.variations.ref, template: Generator.generate(config, entity) }))
-    .reduce((templateOutput: string, { entityName, template }, idx) => {
-      return `${templateOutput} ${idx === 0 ? '' : ','} ${entityName}: { ${JSON.stringify(template)} }`
-    }, '')
+const addSeparator =
+  (separator: string, blank: string) => (predicate: boolean) =>
+    predicate ? "" : separator;
+
+const addCommaSeparator = addSeparator(",", "");
+
+export const mockGeneratorEntity =
+  (Generator: GeneratorEntity) => (config: Config) => ({
+    title: "Mock Data",
+    fileName: `${MODULE.MOCK}/${config.name}mock.json`,
+    template: `{ ${config.entities
+      .map(
+        (entity) =>
+          `"${entity.variations.ref}": [${Generator.generate(config, entity)}]`
+      )
+      .join(",")}}
+    }`,
   });
 
-const MockGenerator: GeneratorEntity = {
-  generate: (_, entity: Schema) => ({
-    fileName: `${MODULE.MOCK}/${entity.variations.models}mock.json`,
-    template: (() => {
-      const template = Array(10).fill('').map(() => {
-        return (entity.fields || []).reduce((acc, { name, type }) => ({ ...acc, [name]: 'test' }), {});
-      })
-      return JSON.stringify(template)
-    })(),
-    title: 'Mock Data'
-  })
+const MockGenerator = {
+  generate: (_, entity: Schema) =>
+    Array(10)
+      .fill("")
+      .map(() => {
+        return (entity.fields || []).reduce(
+          (acc, { name, type }, idx) =>
+            `${acc} ${addCommaSeparator(idx === 0)} "${name}": "test"`,
+          ``
+        );
+      }),
 };
 
 export const mockGenerators = (config: Config) => [
+  //@ts-expect-error
   { func: mockGeneratorEntity(MockGenerator), params: { config } },
-]
+];
--- src/util/commands/package.helpers.ts
+++ src/util/commands/package.helpers.ts
@@ -5,7 +5,9 @@ import { GenerateReturn } from "@interfaces/template.interface";
 export const getZip = () => new JSZip();
 
 export const zipPackageElement = (config, zip) => (element: GenerateReturn) => {
+    console.log('element', element);
     zip.file(element.fileName, element.template);
+    console.log('template', element.template);
     zip.generateAsync({ type: 'arraybuffer' }).then((content) => {
         fs.writeFileSync(`./${config.name}.zip`, Buffer.from(content))
     });
