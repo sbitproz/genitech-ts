8d3facfdd5a9a2576ee7479c5c13abb7fd085931
Dan Clovis <dan@infinatech.co.uk>
2022-03-14 19:12:02 +0000

    Removal of dependencies

--- src/templates/auth/authInterface.templates.ts
+++ src/templates/auth/authInterface.templates.ts
@@ -6,8 +6,6 @@ import { GeneratorEntity } from "@interfaces/template.interface";
 
 const generate = (config: Config) => {
   const template = `
-  import { User, UserCredential } from 'firebase/auth';
-
   export interface BaseUser {
     name: string;
     email: string;
@@ -16,11 +14,11 @@ const generate = (config: Config) => {
   
   export interface AuthService {
     isAuthenticated: () => boolean;
-    login: (username: string, password: string) => Promise<UserCredential>;
+    login: (username: string, password: string) => Promise<any>;
     logout: () => Promise<void>;
     reset: (email: string) => Promise<void>;
-    register: (user: BaseUser) => Promise<UserCredential>;
-    currentUser: () => User | null;
+    register: (user: BaseUser) => Promise<any>;
+    currentUser: () => any | null;
   }
   
   `
