1136e34a03822278be97b1934a209ced0a68d822
Dan Clovis <dan@infinatech.co.uk>
2022-03-15 09:47:42 +0000

    Remove uneeded files

--- mentor-mee-ui/libs/core-ui/src/index.ts
+++
@@ -1,7 +0,0 @@
-
-export * from './lib/theme/colors';
-export * from './lib/theme/global.styles';
-export * from './lib/theme/spacing';
-export * from './lib/theme/theme';
-export * from './lib/theme/types.d';
-  
